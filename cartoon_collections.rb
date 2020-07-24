ce98efa8b00ce6400b7fcbae8597a1a3c7e895c8
Cat Gomez <catherinedgomez@gmail.com>
2020-07-24 01:58:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,7 @@ end
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map(&:capitalize).join("!").split do |call|
     puts call
+  end
 end
 
 def long_planeteer_calls# code an argument here
